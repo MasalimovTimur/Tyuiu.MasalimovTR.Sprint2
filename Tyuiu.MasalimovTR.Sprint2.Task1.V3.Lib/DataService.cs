35e33a7d36da67b35a82b97340b4edeb20eab02f
MasalimovTimur <masalimovtr@gmail.com>
2024-11-15 15:56:09 +0500

    Task1

---
+++ Tyuiu.MasalimovTR.Sprint2.Task1.V3.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MasalimovTR.Sprint2.Task1.V3.Lib
+{
+    public class DataService : ISprint2Task1V3
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = c == d | a <= b;
+            res[1] = d == a & b < c;
+            res[2] = d < c || a > b;
+            res[3] = d == a && b <= c;
+            res[4] = !res[0] ;
+            res[5] = b > c ^ d <= a;  
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MasalimovTR.Sprint2.Task1.V3/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.MasalimovTR.Sprint2.Task1.V3.Lib;
+
+namespace Tyuiu.MasalimovTR.Sprint2.Task1.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Масалимов Т.Р. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                             *");
+            Console.WriteLine("* Выполнил: Масалимов Тимур Рyсланович | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться),  *");
+            Console.WriteLine("* а также арифметических выражений, которая вернет логическую последовательность(массив)      *");
+            Console.WriteLine("* (True, False, False, False, False, False), при a = 185, b = 316, c = 134, d = 134           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Результат:");
+
+            DataService ds = new DataService();
+            int a = 185;
+            int b = 316;
+            int c = 134;
+            int d = 134;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            };
+            Console.ReadKey();
+        }
+    }
+}
