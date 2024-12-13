72f09ce780cf2016b92baa475c5289770b79f539
MasalimovTimur <masalimovtr@gmail.com>
2024-12-14 01:46:25 +0500

    Task4

--- Tyuiu.MasalimovTR.Sprint2.Task4.V12.Lib/Class1.cs
+++ Tyuiu.MasalimovTR.Sprint2.Task4.V12.Lib/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Tyuiu.MasalimovTR.Sprint2.Task4.V12.Lib
-{
-    public class Class1
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
+namespace Tyuiu.MasalimovTR.Sprint2.Task4.V12.Lib
+{
+    public class Class1 : ISprint2Task4V12
+    {
+        public double Calculate(double x, double y)
+        {
+            return 3;
+        }
     }
 }
