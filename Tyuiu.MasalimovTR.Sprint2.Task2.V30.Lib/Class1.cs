0df3414dc3bd09a4f8ed8f0e2b022792b539b764
MasalimovTimur <masalimovtr@gmail.com>
2024-12-14 01:16:08 +0500

    Task2

--- Tyuiu.MasalimovTR.Sprint2.Task2.V30.Lib/Class1.cs
+++ Tyuiu.MasalimovTR.Sprint2.Task2.V30.Lib/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Tyuiu.MasalimovTR.Sprint2.Task2.V30.Lib
-{
-    public class Class1
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
+namespace Tyuiu.MasalimovTR.Sprint2.Task2.V30.Lib
+{
+    public class Class1 : ISprint2Task2V30
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            return true;
+        }
     }
 }
