4cb76885422d014ef77a37921338456e7b2189bd
MasalimovTimur <masalimovtr@gmail.com>
2024-12-14 01:47:56 +0500

    Task5

--- Tyuiu.MasalimovTR.Sprint2.Task5.V1.Lib/Class1.cs
+++ Tyuiu.MasalimovTR.Sprint2.Task5.V1.Lib/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Tyuiu.MasalimovTR.Sprint2.Task5.V1.Lib
-{
-    public class Class1
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
+namespace Tyuiu.MasalimovTR.Sprint2.Task5.V1.Lib
+{
+    public class Class1 : ISprint2Task5V1
+    {
+        public int FindMonthDaysCount(int value)
+        {
+            return 3;
+        }
     }
 }
